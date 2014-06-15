87c773a350a0f5720a49e4f95b7d7a3a851d4d7e
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-15 13:26:08 -0500

    about to delete the page that just displays the long lat

--- camerataker/ViewController.swift
+++ camerataker/ViewController.swift
@@ -7,31 +7,56 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate , UITextFieldDelegate, CLLocationManagerDelegate {
     
+    // init location manager and set coordinates to 0
+    let locationManager = CLLocationManager()
+    var myLong = 0.0
+    var myLat = 0.0
+    
+    // after the view loads, start getting location
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
+    
+    // standard
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    // tag textfield, image, latitude, and longitude with variable names
+    @IBOutlet var textTask: UITextField!
+    @IBOutlet var latitudeLabel : UILabel
+    @IBOutlet var longitudeLabel : UILabel
     @IBOutlet var imageView : UIImageView = nil
-
+    
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        self.view.endEditing(true)
+    }
+    
+    // textfield delegate removes keyboard when return is hit
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // opens the camera when you git the "Take Photo" button until "Use Photo" is confirmed
+    // the the camera closes
     @IBAction func takePhoto(sender : UIButton) {
         var image = UIImagePickerController()
         image.delegate = self
         image.sourceType = UIImagePickerControllerSourceType.Camera
-        //image.mediaTypes = [EXIF]
         image.allowsEditing = false
         self.presentViewController(image, animated: true, completion: nil)
     }
     
-    
+    // saves the photo to the variable
     func imagePickerController(image: UIImagePickerController, didFinishPickingMediaWithInfo info: NSDictionary){
         println(image)
         println(info)
@@ -41,8 +66,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.dismissModalViewControllerAnimated(true)
     }
     
-    @IBAction func getCurrentLocation(sender : UIButton) {
-      
+    // sets the actual long and lat values to the variables and convert to strings with 6 deceimal places
+    func locationManager(manager:CLLocationManager!, didUpdateLocations locations:AnyObject[]) {
+        myLong = locations[0].coordinate.latitude
+        myLat = locations[0].coordinate.longitude
+        
+        var answerlat = ""
+        var answerlong = ""
+        var counterlat = 0
+        var counterlong = 0
+        
+        for x in "\(myLat)" {
+            if x == "." { counterlat = 1 }
+            if counterlat < 8 { answerlat += x }
+            counterlat += 1
+        }
+        
+        for x in "\(myLong)" {
+            if x == "." { counterlong = 1 }
+            if counterlong < 8 { answerlong += x }
+            counterlong += 1
+        }
+        
+        longitudeLabel.text = answerlat
+        latitudeLabel.text = answerlong
+        println(answerlat)
+        println(answerlong)
+    }
+
+    // submit memory button
+    @IBAction func btnCaptureMem(sender : UIButton) {
+        println("Button was clicked")
+        println(textTask.text)
+        self.view.endEditing(true)
+        textTask.text = ""
+        // self.tabBarController.selectedIndex = 0
+        // INSET POP UP HERE THAT MEM WAS SAVED
+        // AND MOVE USER TO HOME PAGE
+        
     }
     
 }
