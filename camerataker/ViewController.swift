574ae2dc2e609b6091c67105b3ee10e545e24b81
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-14 20:38:55 -0500

    camera working

--- camerataker/ViewController.swift
+++ camerataker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var imageView : UIImageView = nil
+
+    @IBAction func takePhoto(sender : UIButton) {
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.Camera
+        image.allowsEditing = false
+        self.presentViewController(image, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(image: UIImagePickerController, didFinishPickingMediaWithInfo info: NSDictionary){
+        println(image)
+        println(info)
+        var chosenImage: UIImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        self.imageView.image = chosenImage
+        println("pop")
+        
+        self.dismissModalViewControllerAnimated(true)
+
+        
+        
+        
+    }
 
 }
 
